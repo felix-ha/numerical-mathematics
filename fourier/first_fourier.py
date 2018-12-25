92db65d52769f643b2b8a3dba9a5f04ddfffe2e2
felix hauer <felix.hauer@evopro-ag.de>
2018-12-25 16:40:42 +0100

    splitted functions into modules

--- fourier/first_fourier.py
+++ fourier/first_fourier.py
@@ -1,89 +1,12 @@
 import numpy as np
+from functions import rect
+from fourier import fourier_coeff, calculate_fourier_series, fourier_tranformation
 from matplotlib import pyplot as plt
 plt.style.use('ggplot')
 
-
-def ear(t):
-    return 1.273*np.sin(2*np.pi * 128 * t) + 0.424*np.sin(2*np.pi * 384 * t)+ 0.255*np.sin(2*np.pi * 640 * t)
-
-def f(x):
-    return np.sin(x)
-
-
-def rect(x):
-    if x < 0:
-        x = np.absolute(x) + np.pi
-
-    if x > 2 * np.pi:
-        n = int(x / (2 * np.pi))
-        x = x - n * 2 * np.pi
-
-    if 0 < x < np.pi:
-        return 1
-    elif x == 0 or x == np.pi or x == np.pi * 2:
-        return 0
-    else:
-        return -1
-
-
-
-def integral(f, a, b):
-    h = 0.001
-    s = 0
-    x_current = a
-
-    while x_current < b:
-        xi = x_current + h / 2
-        s += f(xi) * h
-        x_current += h
-
-    return s
-
-def fourier(f, xi):
-    def _f(x):
-        return f(x) * np.exp(2*np.pi * 1j * x * xi)
-    return integral(_f, -2, 2)
-
-def fourier_coeff(f, T, N):
-    a = np.empty(N+1, dtype=np.float)
-    a[0] = (1 / T) * integral(f, 0, T)
-
-    for i in range(N):
-        def _f(t):
-            return f(t) * np.cos((i+1) * (2*np.pi / T) * t)
-        a[i+1] = (2 / T) * integral(_f, 0, T)
-
-    b = np.empty(N, dtype=np.float)
-    for i in range(N):
-        def _f(t):
-            return f(t) * np.sin((i+1) * (2*np.pi / T) * t)
-        b[i] = (2 / T) * integral(_f, 0, T)
-
-    return a, b
-
-def calculate_fourier_series(t, a, b, T):
-    N = len(b)
-    f = a[0]
-
-    cos = 0
-    for n in range(1,N+1):
-        cos += a[n] * np.cos(n * np.pi * 2 * t / T)
-    f += cos
-
-    sin = 0
-    for n in range(1,N+1):
-        sin += b[n-1] * np.sin(n * np.pi * 2 * t / T)
-    f += sin
-
-    return f
-
-
-if __name__ == '__main__':
-    N = 5
+def fourier_series_rect():
+    N = 10
     f = rect
-
-    n_a = range(N+1)
-    n_b = range(N)
 
     T = 2 * np.pi
     a, b = fourier_coeff(f, T, N)
@@ -92,12 +15,60 @@ if __name__ == '__main__':
     f_x = [f(z) for z in x]
     f_fourier_series = [calculate_fourier_series(z, a, b, T) for z in x]
 
+
+    plt.subplot(2,1,1)
     plt.plot(x, f_x)
     plt.plot(x, f_fourier_series)
-    plt.savefig('fourier.png', dpi=500)
+    plt.legend(['f', 'f series of f'])
+    plt.title('function vs fourier series')
 
+
+    #plot coeff
+
+    plt.subplot(2,1,2)
+    n_a = range(N+1)
+    n_b = range(N)
+
+    plt.plot(n_a, a)
     plt.plot(n_b, b)
+    plt.legend(['a', 'b'])
+    plt.title('fourier coeffiecents')
+
+
+
+    plt.savefig('fourier.png', dpi=500)
     plt.show()
+
+
+def fourier_transormation_rect():
+    f = rect
+
+    x = np.arange(-6, 6, 0.01)
+    f_x = [f(z) for z in x]
+    f_fourier_trafo = [fourier_tranformation(f, z) for z in x]
+
+    plt.subplot(2,1,1)
+    plt.plot(x, f_x)
+    plt.title('function')
+    plt.ylim([-1.5, 1.5])
+
+
+    plt.subplot(2,1,2)
+    plt.plot(x, np.real(f_fourier_trafo))
+    plt.plot(x, np.imag(f_fourier_trafo))
+    plt.title('fourier transformation')
+    plt.legend(['real', 'imag'])
+
+
+
+    plt.savefig('fourier.png', dpi=500)
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    # fourier_series_rect()
+    fourier_transormation_rect()
 
 
 
